3c08d55ff02d8facad54f5189bf721e01ab9448f
Guilherme Henrique <guilhermeerba@hotmail.com>
2019-03-03 14:22:30 -0300

    Função de Gerar Strings Aleatorias Criada

--- main.js
+++ main.js
@@ -17,4 +17,29 @@ client.on('guildDelete', guild => {
   client.user.setActivity(`Serving ${clients.guilds.size} Servers`);
 });
 
+client.on('message', async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+
+  // First Command
+  if(comando === 'quemsou') {
+    let r_text = new Array ();
+    r_text[0] = '1';
+    r_text[1] = '2';
+    r_text[2] = '3';
+    r_text[3] = '4';
+    r_text[4] = '5';
+    r_text[5] = '6';
+    r_text[6] = '7';
+    let i = Math.floor(7*Math.random());
+
+    const quemsou = await message.channel.send('calma');
+    quemsou.edit(r_text[i]);
+  }
+});
+
 client.login(config.token);
