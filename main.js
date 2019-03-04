233e7e28cd14f6591ff5b16080dd77cce7fb1e3f
Guilherme Henrique <guilhermeerba@hotmail.com>
2019-03-03 22:48:45 -0300

    Img corrigido

--- main.js
+++ main.js
@@ -45,21 +45,20 @@ client.on('message', async message => {
     quemsou.edit(`${message.author}, `);
     */
 
-   const attachment = new Attachment('./rip.png');
+   const attachment = new Attachment('./meme.png');
 
    let r_text = new Array ();
-   r_text[0] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[1] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[2] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[3] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[4] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[5] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
-   r_text[6] = (`${message.author} é um Lolzeiro, sai daqui`, attachment);
+   r_text[0] = `${message.author} é um Lolzeiro, sai daqui`;
+    r_text[1] = `${message.author} é um C O D I S T A safado`;
+    r_text[2] = `${message.author} já soltou sua ULT da Mercy hj? Patetico...`;
+    r_text[3] = `${message.author} só joga com ASH e acha que é PRO`;
+    r_text[4] = `${message.author} Famoso Global carregado, kjjjj`;
+    r_text[5] = `${message.author} joga PUBG com 15 FPS e ainda acha divertido, kk eae men`;
+    r_text[6] = `${message.author} é jogador de FOGO GRÁTIS, aqui você tem acesso livre e permissão de ADMINISTRATOR, sejá bem-vindo MEU REI!`;
 
    let i = Math.floor(7*Math.random());
 
-   //message.channel.send(`${r_text[i]},`, attachment);
-   message.channel.send(r_text[i]);
+   message.channel.send(`${r_text[i]}`, attachment);
   }
 });
 
